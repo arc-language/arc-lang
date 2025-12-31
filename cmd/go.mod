1e2f4a00dcd7b4c1f0ba6b979f4997ecd6e154f5
Arc Language <danielv4857@gmail.com>
2025-12-31 05:31:21 +0000

    updates

--- cmd/go.mod
+++ cmd/go.mod
@@ -4,6 +4,6 @@ go 1.25.4
 
 require (
 	github.com/antlr4-go/antlr/v4 v4.13.1 // indirect
-	github.com/arc-language/arc-lang v0.0.0-20251231052851-f16f9bb6fd72 // indirect
+	github.com/arc-language/arc-lang v0.0.0-20251231052930-8826a68e8abb // indirect
 	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
 )
--- semantics/helpers.go
+++ semantics/helpers.go
@@ -2,12 +2,17 @@ package semantics
 
 import (
 	"strconv"
+
 	"github.com/arc-language/arc-lang/builder/types"
 	"github.com/arc-language/arc-lang/parser"
 	"github.com/arc-language/arc-lang/symbol"
 )
 
+// resolveType converts AST Type nodes into internal Type representations
 func (a *Analyzer) resolveType(ctx parser.ITypeContext) types.Type {
+	if ctx == nil {
+		return types.Void
+	}
 	tc := ctx.(*parser.TypeContext)
 
 	if tc.PrimitiveType() != nil {
@@ -15,6 +20,8 @@ func (a *Analyzer) resolveType(ctx parser.ITypeContext) types.Type {
 		if s, ok := a.currentScope.Resolve(name); ok && s.Kind == symbol.SymType {
 			return s.Type
 		}
+		// Default to I64 if unknown to prevent crashes, but log error
+		a.bag.Report(a.file, tc.GetStart().GetLine(), 0, "Unknown primitive type '%s'", name)
 		return types.I64
 	}
 	
@@ -25,8 +32,10 @@ func (a *Analyzer) resolveType(ctx parser.ITypeContext) types.Type {
 	if tc.ArrayType() != nil {
 		elem := a.resolveType(tc.ArrayType().Type_())
 		size := int64(0)
-		if s := tc.ArrayType().ArraySize(); s != nil && s.INTEGER_LITERAL() != nil {
-			size, _ = strconv.ParseInt(s.INTEGER_LITERAL().GetText(), 10, 64)
+		if s := tc.ArrayType().ArraySize(); s != nil {
+			if s.INTEGER_LITERAL() != nil {
+				size, _ = strconv.ParseInt(s.INTEGER_LITERAL().GetText(), 0, 64)
+			}
 		}
 		return types.NewArray(elem, size)
 	}
@@ -36,7 +45,25 @@ func (a *Analyzer) resolveType(ctx parser.ITypeContext) types.Type {
 		if s, ok := a.currentScope.Resolve(name); ok && s.Kind == symbol.SymType {
 			return s.Type
 		}
+		a.bag.Report(a.file, tc.GetStart().GetLine(), 0, "Unknown type '%s'", name)
 	}
 	
 	return types.I64
+}
+
+// areTypesCompatible checks if two types can be used together (e.g. assignment, math)
+func areTypesCompatible(src, dest types.Type) bool {
+	if src.Equal(dest) {
+		return true
+	}
+	// Implicit int casting (e.g. int32 -> int64)
+	if types.IsInteger(src) && types.IsInteger(dest) {
+		return true 
+	}
+	// Implicit float casting
+	if types.IsFloat(src) && types.IsFloat(dest) {
+		return true
+	}
+	
+	return false
 }
