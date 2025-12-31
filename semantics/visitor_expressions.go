6ab83165f561e7edc2a99910856798355ff9d9d1
Arc Language <danielv4857@gmail.com>
2025-12-31 06:13:04 +0000

    updates

--- semantics/visitor_expressions.go
+++ semantics/visitor_expressions.go
@@ -5,16 +5,126 @@ import (
 	"github.com/arc-language/arc-lang/parser"
 )
 
-// ... (Arithmetic/Logic visitors same as before) ...
+// --- Expressions ---
+
+func (a *Analyzer) VisitExpression(ctx *parser.ExpressionContext) interface{} {
+	if ctx.LogicalOrExpression() != nil {
+		t := a.Visit(ctx.LogicalOrExpression()).(types.Type)
+		a.nodeTypes[ctx] = t
+		return t
+	}
+	return types.Void
+}
+
+// --- Binary Ops ---
+
+func (a *Analyzer) VisitAdditiveExpression(ctx *parser.AdditiveExpressionContext) interface{} {
+	lhs := a.Visit(ctx.MultiplicativeExpression(0)).(types.Type)
+	for i := 1; i < len(ctx.AllMultiplicativeExpression()); i++ {
+		// We just visit the RHS to ensure it resolves, validation happens implicitly via lookup
+		a.Visit(ctx.MultiplicativeExpression(i))
+	}
+	return lhs
+}
+
+func (a *Analyzer) VisitMultiplicativeExpression(ctx *parser.MultiplicativeExpressionContext) interface{} {
+	lhs := a.Visit(ctx.UnaryExpression(0)).(types.Type)
+	for i := 1; i < len(ctx.AllUnaryExpression()); i++ {
+		a.Visit(ctx.UnaryExpression(i))
+	}
+	return lhs
+}
+
+// --- Unary Ops ---
+
+func (a *Analyzer) VisitUnaryExpression(ctx *parser.UnaryExpressionContext) interface{} {
+	if ctx.PostfixExpression() != nil {
+		return a.Visit(ctx.PostfixExpression())
+	}
+	
+	val := a.Visit(ctx.UnaryExpression()).(types.Type)
+	
+	if ctx.AMP() != nil {
+		return types.NewPointer(val)
+	}
+	
+	if ctx.STAR() != nil {
+		if ptr, ok := val.(*types.PointerType); ok {
+			return ptr.ElementType
+		}
+	}
+	
+	return val
+}
+
+// --- Postfix (Calls, Index, Members) ---
+
+func (a *Analyzer) VisitPostfixExpression(ctx *parser.PostfixExpressionContext) interface{} {
+	// Start with the base type
+	curr := a.Visit(ctx.PrimaryExpression()).(types.Type)
+	
+	for _, op := range ctx.AllPostfixOp() {
+		// 1. Member Access (.field)
+		if op.DOT() != nil && op.IDENTIFIER() != nil {
+			fieldName := op.IDENTIFIER().GetText()
+			
+			// Auto-dereference pointer
+			if ptr, ok := curr.(*types.PointerType); ok {
+				curr = ptr.ElementType
+			}
+			
+			if st, ok := curr.(*types.StructType); ok {
+				if indices, ok := a.structIndices[st.Name]; ok {
+					if idx, ok := indices[fieldName]; ok {
+						curr = st.Fields[idx]
+						continue
+					}
+				}
+				// If not found, default to generic int to prevent cascade errors
+				// In a real compiler, we'd log an error here.
+			}
+		}
+
+		// 2. Indexing ([expr])
+		if op.LBRACKET() != nil {
+			// Visit index to ensure it resolves
+			a.Visit(op.Expression())
+			
+			if ptr, ok := curr.(*types.PointerType); ok {
+				curr = ptr.ElementType
+			} else if arr, ok := curr.(*types.ArrayType); ok {
+				curr = arr.ElementType
+			}
+		}
+		
+		// 3. Calls ((args))
+		if op.LPAREN() != nil {
+			// Stub: If we tracked function signatures, we would return the return type.
+			// Since we simplified Symbol.Type to just be the return type for functions,
+			// 'curr' should already be the return type if it came from a function identifier.
+			// However, if we are doing `myFunc()`, curr starts as the func type (RetType).
+			// So we technically don't need to change `curr` unless we implement full function types.
+		}
+	}
+	
+	return curr
+}
+
+// --- Primary ---
 
 func (a *Analyzer) VisitPrimaryExpression(ctx *parser.PrimaryExpressionContext) interface{} {
-	if ctx.Literal() != nil { return a.Visit(ctx.Literal()) }
+	if ctx.Literal() != nil {
+		return a.Visit(ctx.Literal())
+	}
 	
+	// Qualified (io.printf)
 	if ctx.QualifiedIdentifier() != nil {
 		q := ctx.QualifiedIdentifier()
 		parts := ""
 		for i, id := range q.AllIDENTIFIER() {
-			if i > 0 { parts += "." }
+			if i > 0 {
+				parts += "."
+			}
 			parts += id.GetText()
 		}
 		if sym, ok := a.currentScope.Resolve(parts); ok {
@@ -26,80 +136,62 @@ func (a *Analyzer) VisitPrimaryExpression(ctx *parser.PrimaryExpressionContext) 
 
 	if ctx.IDENTIFIER() != nil {
 		name := ctx.IDENTIFIER().GetText()
-		if s, ok := a.currentScope.Resolve(name); ok { return s.Type }
-		a.bag.Report(a.file, ctx.GetStart().GetLine(), 0, "Undefined %s", name)
+		if s, ok := a.currentScope.Resolve(name); ok {
+			return s.Type
+		}
+		a.bag.Report(a.file, ctx.GetStart().GetLine(), 0, "Undefined '%s'", name)
+		return types.Void
 	}
-	if ctx.Expression() != nil { return a.Visit(ctx.Expression()) }
+	
+	if ctx.Expression() != nil {
+		return a.Visit(ctx.Expression())
+	}
+	
 	return types.Void
 }
 
-// ... (Rest of expression visitors same as before) ...
-func (a *Analyzer) VisitExpression(ctx *parser.ExpressionContext) interface{} {
-	if ctx.LogicalOrExpression() != nil {
-		t := a.Visit(ctx.LogicalOrExpression()).(types.Type)
-		a.nodeTypes[ctx] = t
-		return t
-	}
-	return types.Void
-}
-func (a *Analyzer) VisitAdditiveExpression(ctx *parser.AdditiveExpressionContext) interface{} {
-	lhs := a.Visit(ctx.MultiplicativeExpression(0)).(types.Type)
-	for i := 1; i < len(ctx.AllMultiplicativeExpression()); i++ {
-		a.Visit(ctx.MultiplicativeExpression(i))
-	}
-	return lhs
-}
-func (a *Analyzer) VisitMultiplicativeExpression(ctx *parser.MultiplicativeExpressionContext) interface{} {
-	lhs := a.Visit(ctx.UnaryExpression(0)).(types.Type)
-	for i := 1; i < len(ctx.AllUnaryExpression()); i++ {
-		a.Visit(ctx.UnaryExpression(i))
-	}
-	return lhs
-}
-func (a *Analyzer) VisitUnaryExpression(ctx *parser.UnaryExpressionContext) interface{} {
-	if ctx.PostfixExpression() != nil { return a.Visit(ctx.PostfixExpression()) }
-	val := a.Visit(ctx.UnaryExpression()).(types.Type)
-	if ctx.AMP() != nil { return types.NewPointer(val) }
-	if ctx.STAR() != nil {
-		if ptr, ok := val.(*types.PointerType); ok { return ptr.ElementType }
-	}
-	return val
-}
-func (a *Analyzer) VisitPostfixExpression(ctx *parser.PostfixExpressionContext) interface{} {
-	curr := a.Visit(ctx.PrimaryExpression()).(types.Type)
-	for _, op := range ctx.AllPostfixOp() {
-		if op.DOT() != nil && op.IDENTIFIER() != nil {
-			fieldName := op.IDENTIFIER().GetText()
-			if ptr, ok := curr.(*types.PointerType); ok { curr = ptr.ElementType }
-			if st, ok := curr.(*types.StructType); ok {
-				if indices, ok := a.structIndices[st.Name]; ok {
-					if idx, ok := indices[fieldName]; ok {
-						curr = st.Fields[idx]
-						continue
-					}
-				}
-			}
-		}
-		if op.LBRACKET() != nil {
-			if ptr, ok := curr.(*types.PointerType); ok { curr = ptr.ElementType }
-			else if arr, ok := curr.(*types.ArrayType); ok { curr = arr.ElementType }
-		}
-	}
-	return curr
-}
 func (a *Analyzer) VisitLiteral(ctx *parser.LiteralContext) interface{} {
-	if ctx.INTEGER_LITERAL() != nil { return types.I64 }
-	if ctx.FLOAT_LITERAL() != nil { return types.F64 }
-	if ctx.BOOLEAN_LITERAL() != nil { return types.I1 }
-	if ctx.STRING_LITERAL() != nil { return types.NewPointer(types.I8) }
+	if ctx.INTEGER_LITERAL() != nil {
+		return types.I64
+	}
+	if ctx.FLOAT_LITERAL() != nil {
+		return types.F64
+	}
+	if ctx.BOOLEAN_LITERAL() != nil {
+		return types.I1
+	}
+	if ctx.STRING_LITERAL() != nil {
+		return types.NewPointer(types.I8)
+	}
 	return types.Void
 }
-func (a *Analyzer) VisitLogicalOrExpression(ctx *parser.LogicalOrExpressionContext) interface{} { return a.Visit(ctx.LogicalAndExpression(0)) }
-func (a *Analyzer) VisitLogicalAndExpression(ctx *parser.LogicalAndExpressionContext) interface{} { return a.Visit(ctx.BitOrExpression(0)) }
-func (a *Analyzer) VisitBitOrExpression(ctx *parser.BitOrExpressionContext) interface{} { return a.Visit(ctx.BitXorExpression(0)) }
-func (a *Analyzer) VisitBitXorExpression(ctx *parser.BitXorExpressionContext) interface{} { return a.Visit(ctx.BitAndExpression(0)) }
-func (a *Analyzer) VisitBitAndExpression(ctx *parser.BitAndExpressionContext) interface{} { return a.Visit(ctx.EqualityExpression(0)) }
-func (a *Analyzer) VisitEqualityExpression(ctx *parser.EqualityExpressionContext) interface{} { return a.Visit(ctx.RelationalExpression(0)) }
-func (a *Analyzer) VisitRelationalExpression(ctx *parser.RelationalExpressionContext) interface{} { return a.Visit(ctx.ShiftExpression(0)) }
-func (a *Analyzer) VisitShiftExpression(ctx *parser.ShiftExpressionContext) interface{} { return a.Visit(ctx.RangeExpression(0)) }
-func (a *Analyzer) VisitRangeExpression(ctx *parser.RangeExpressionContext) interface{} { return a.Visit(ctx.AdditiveExpression(0)) }
+
+// --- Boilerplate Dispatch ---
+
+func (a *Analyzer) VisitLogicalOrExpression(ctx *parser.LogicalOrExpressionContext) interface{} {
+	return a.Visit(ctx.LogicalAndExpression(0))
+}
+func (a *Analyzer) VisitLogicalAndExpression(ctx *parser.LogicalAndExpressionContext) interface{} {
+	return a.Visit(ctx.BitOrExpression(0))
+}
+func (a *Analyzer) VisitBitOrExpression(ctx *parser.BitOrExpressionContext) interface{} {
+	return a.Visit(ctx.BitXorExpression(0))
+}
+func (a *Analyzer) VisitBitXorExpression(ctx *parser.BitXorExpressionContext) interface{} {
+	return a.Visit(ctx.BitAndExpression(0))
+}
+func (a *Analyzer) VisitBitAndExpression(ctx *parser.BitAndExpressionContext) interface{} {
+	return a.Visit(ctx.EqualityExpression(0))
+}
+func (a *Analyzer) VisitEqualityExpression(ctx *parser.EqualityExpressionContext) interface{} {
+	return a.Visit(ctx.RelationalExpression(0))
+}
+func (a *Analyzer) VisitRelationalExpression(ctx *parser.RelationalExpressionContext) interface{} {
+	return a.Visit(ctx.ShiftExpression(0))
+}
+func (a *Analyzer) VisitShiftExpression(ctx *parser.ShiftExpressionContext) interface{} {
+	return a.Visit(ctx.RangeExpression(0))
+}
+func (a *Analyzer) VisitRangeExpression(ctx *parser.RangeExpressionContext) interface{} {
+	return a.Visit(ctx.AdditiveExpression(0))
+}
