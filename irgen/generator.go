718e7756f423d4e749b8968af4072f173eb3c2a1
Arc Language <danielv4857@gmail.com>
2025-12-31 05:36:34 +0000

    updates

--- irgen/generator.go
+++ irgen/generator.go
@@ -3,7 +3,6 @@ package irgen
 import (
 	"github.com/antlr4-go/antlr/v4"
 	"github.com/arc-language/arc-lang/builder/ir"
-	"github.com/arc-language/arc-lang/builder/types"
 	"github.com/arc-language/arc-lang/context"
 	"github.com/arc-language/arc-lang/parser"
 	"github.com/arc-language/arc-lang/semantics"
@@ -16,7 +15,9 @@ type Generator struct {
 	analysis     *semantics.AnalysisResult
 	currentScope *symbol.Scope
 	deferStack   *DeferStack
-	loopStack    []loopInfo
+	
+	// Loop stack for break/continue
+	loopStack []loopInfo
 }
 
 type loopInfo struct {
@@ -38,51 +39,13 @@ func Generate(tree parser.ICompilationUnitContext, moduleName string, analysis *
 }
 
 func (g *Generator) enterScope(ctx antlr.ParserRuleContext) {
-	if s, ok := g.analysis.Scopes[ctx]; ok { g.currentScope = s }
+	if s, ok := g.analysis.Scopes[ctx]; ok {
+		g.currentScope = s
+	}
 }
 
 func (g *Generator) exitScope() {
-	if g.currentScope.Parent != nil { g.currentScope = g.currentScope.Parent }
-}
-
-func (g *Generator) getZeroValue(t types.Type) ir.Value {
-	if types.IsInteger(t) { return g.ctx.Builder.ConstInt(t.(*types.IntType), 0) }
-	if types.IsFloat(t) { return g.ctx.Builder.ConstFloat(t.(*types.FloatType), 0.0) }
-	return g.ctx.Builder.ConstZero(t)
-}
-
-func (g *Generator) emitCast(val ir.Value, target types.Type) ir.Value {
-	src := val.Type()
-	if src.Equal(target) { return val }
-	if types.IsInteger(src) && types.IsInteger(target) {
-		if src.BitSize() > target.BitSize() { return g.ctx.Builder.CreateTrunc(val, target, "") }
-		return g.ctx.Builder.CreateSExt(val, target, "")
+	if g.currentScope.Parent != nil {
+		g.currentScope = g.currentScope.Parent
 	}
-	if types.IsFloat(src) && types.IsFloat(target) {
-		if src.BitSize() > target.BitSize() { return g.ctx.Builder.CreateFPTrunc(val, target, "") }
-		return g.ctx.Builder.CreateFPExt(val, target, "")
-	}
-	if types.IsInteger(src) && types.IsFloat(target) { return g.ctx.Builder.CreateSIToFP(val, target, "") }
-	return val
-}
-
-func (g *Generator) resolveType(ctx parser.ITypeContext) types.Type {
-	tc := ctx.(*parser.TypeContext)
-	if tc.PrimitiveType() != nil {
-		switch tc.PrimitiveType().GetText() {
-		case "int": return types.I64
-		case "float": return types.F64
-		case "bool": return types.I1
-		case "void": return types.Void
-		}
-	}
-	if tc.PointerType() != nil {
-		return types.NewPointer(g.resolveType(tc.PointerType().Type_()))
-	}
-	if tc.IDENTIFIER() != nil {
-		// Lookup struct types created in Pass 1
-		name := tc.IDENTIFIER().GetText()
-		if s, ok := g.currentScope.Resolve(name); ok { return s.Type }
-	}
-	return types.I64
 }
--- irgen/helpers.go
+++ irgen/helpers.go
@@ -7,20 +7,35 @@ import (
 )
 
 // resolveType converts AST type to IR type
-// In a full implementation, this should look up the SymbolTable to find Type Symbols
 func (g *Generator) resolveType(ctx parser.ITypeContext) types.Type {
 	tc := ctx.(*parser.TypeContext)
-	
 	if tc.PrimitiveType() != nil {
-		txt := tc.PrimitiveType().GetText()
-		switch txt {
+		switch tc.PrimitiveType().GetText() {
 		case "int": return types.I64
+		case "int32": return types.I32
 		case "float": return types.F64
 		case "bool": return types.I1
 		case "void": return types.Void
+		case "string": return types.NewPointer(types.I8)
 		}
 	}
-	// Fallback
+	if tc.PointerType() != nil {
+		return types.NewPointer(g.resolveType(tc.PointerType().Type_()))
+	}
+	if tc.ArrayType() != nil {
+		// Basic array support
+		elem := g.resolveType(tc.ArrayType().Type_())
+		// Note: Actual length parsing would happen here if we supported fixed-size arrays in IR directly
+		// For now returning a pointer or slice equivalent is common, but let's stick to fixed
+		return types.NewPointer(elem) 
+	}
+	if tc.IDENTIFIER() != nil {
+		// Lookup struct types created in Pass 1
+		name := tc.IDENTIFIER().GetText()
+		if s, ok := g.currentScope.Resolve(name); ok { 
+			return s.Type 
+		}
+	}
 	return types.I64
 }
 
@@ -28,7 +43,6 @@ func (g *Generator) resolveType(ctx parser.ITypeContext) types.Type {
 func (g *Generator) emitCast(val ir.Value, target types.Type) ir.Value {
 	src := val.Type()
 	
-	// Identity cast
 	if src.Equal(target) {
 		return val
 	}
@@ -38,6 +52,7 @@ func (g *Generator) emitCast(val ir.Value, target types.Type) ir.Value {
 		if src.BitSize() > target.BitSize() {
 			return g.ctx.Builder.CreateTrunc(val, target, "")
 		} else if src.BitSize() < target.BitSize() {
+			// Defaulting to Sign Extension for safety
 			return g.ctx.Builder.CreateSExt(val, target, "")
 		}
 	}
@@ -60,4 +75,18 @@ func (g *Generator) emitCast(val ir.Value, target types.Type) ir.Value {
 	}
 
 	return val
+}
+
+// getZeroValue returns a constant zero for a given type
+func (g *Generator) getZeroValue(t types.Type) ir.Value {
+	if types.IsInteger(t) {
+		return g.ctx.Builder.ConstInt(t.(*types.IntType), 0)
+	}
+	if types.IsFloat(t) {
+		return g.ctx.Builder.ConstFloat(t.(*types.FloatType), 0.0)
+	}
+	if types.IsPointer(t) {
+		return g.ctx.Builder.ConstNull(t.(*types.PointerType))
+	}
+	return g.ctx.Builder.ConstZero(t)
 }
