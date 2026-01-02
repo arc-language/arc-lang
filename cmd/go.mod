addbe273e900e161013f1daea70f0b6124813990
Arc Language <danielv4857@gmail.com>
2026-01-02 11:40:42 +0000

    updates

--- cmd/go.mod
+++ cmd/go.mod
@@ -4,6 +4,6 @@ go 1.25.4
 
 require (
 	github.com/antlr4-go/antlr/v4 v4.13.1 // indirect
-	github.com/arc-language/arc-lang v0.0.0-20260102112814-4e2643e35226 // indirect
+	github.com/arc-language/arc-lang v0.0.0-20260102113634-af2c30ad10d2 // indirect
 	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
 )
--- irgen/lvalue.go
+++ irgen/lvalue.go
@@ -60,11 +60,106 @@ func (g *Generator) getLValue(tree antlr.ParseTree) ir.Value {
 			return g.getLValue(ctx.UnaryExpression(0))
 		}
 
+	// --- LeftHandSide ---
+	case *parser.LeftHandSideContext:
+		// IDENTIFIER
+		if ctx.IDENTIFIER() != nil && ctx.DOT() == nil && ctx.STAR() == nil && ctx.LBRACKET() == nil {
+			name := ctx.IDENTIFIER().GetText()
+			if sym, ok := g.currentScope.Resolve(name); ok {
+				if alloca, ok := sym.IRValue.(*ir.AllocaInst); ok {
+					return alloca
+				}
+				if glob := g.ctx.Module.GetGlobal(name); glob != nil {
+					return glob
+				}
+				return sym.IRValue
+			}
+			return nil
+		}
+
+		// STAR postfixExpression
+		if ctx.STAR() != nil {
+			// Explicit dereference: *ptr
+			val := g.Visit(ctx.PostfixExpression()).(ir.Value)
+			// The value of 'ptr' IS the address we want to write to
+			return val
+		}
+
+		// postfixExpression DOT IDENTIFIER
+		if ctx.DOT() != nil {
+			base := g.getLValue(ctx.PostfixExpression())
+			if base == nil {
+				// If base is not an l-value (e.g. function call return), we can't assign to field?
+				// Actually, we might be able to if we loaded it? No, assignments require address.
+				// Try visiting to get the pointer value if it's a pointer type?
+				val := g.Visit(ctx.PostfixExpression()).(ir.Value)
+				if types.IsPointer(val.Type()) {
+					base = val
+				}
+			}
+			
+			if base != nil {
+				fieldName := ctx.IDENTIFIER().GetText()
+				ptrType := base.Type().(*types.PointerType)
+				if st, ok := ptrType.ElementType.(*types.StructType); ok {
+					if idx, ok := g.analysis.StructIndices[st.Name][fieldName]; ok {
+						return g.ctx.Builder.CreateStructGEP(st, base, idx, "")
+					}
+				}
+			}
+			return nil
+		}
+
+		// postfixExpression LBRACKET expression RBRACKET
+		if ctx.LBRACKET() != nil {
+			idxVal := g.Visit(ctx.Expression()).(ir.Value)
+			
+			// Get the base address.
+			// 1. Try getLValue (if it's an array variable)
+			base := g.getLValue(ctx.PostfixExpression())
+			
+			// 2. If nil, try Visit (if it's a pointer value, e.g. from function call or pointer variable)
+			if base == nil {
+				val := g.Visit(ctx.PostfixExpression()).(ir.Value)
+				if types.IsPointer(val.Type()) {
+					// It's a pointer value, so that IS the base address
+					base = val
+				}
+			} else {
+				// If we got an l-value, we need to load it if it's a pointer variable (double pointer)
+				// OR if it's an array, decay it.
+				// `getLValue` returns the address of the variable.
+				// If `arr` is `*i32` (variable), `getLValue` returns `**i32`.
+				// If `arr` is `[5 x i32]`, `getLValue` returns `*[5 x i32]`.
+				
+				ptrType := base.Type().(*types.PointerType)
+				
+				// Case A: Variable holding a pointer (*int32)
+				if ptrToPtr, ok := ptrType.ElementType.(*types.PointerType); ok {
+					_ = ptrToPtr
+					// Load the actual pointer value
+					base = g.ctx.Builder.CreateLoad(ptrType.ElementType, base, "")
+				}
+				// Case B: Array (don't load, just GEP)
+			}
+
+			if base != nil {
+				ptrType := base.Type().(*types.PointerType)
+				
+				if _, isArray := ptrType.ElementType.(*types.ArrayType); isArray {
+					zero := g.ctx.Builder.ConstInt(types.I32, 0)
+					return g.ctx.Builder.CreateInBoundsGEP(ptrType.ElementType, base, []ir.Value{zero, idxVal}, "")
+				} else {
+					return g.ctx.Builder.CreateInBoundsGEP(ptrType.ElementType, base, []ir.Value{idxVal}, "")
+				}
+			}
+			return nil
+		}
+
 	// --- Actual L-Value Logic ---
 
 	case *parser.PrimaryExpressionContext:
 		// Function calls are NOT l-values. 
-		// Check if parens are present (indicating a call).
 		if ctx.LPAREN() != nil {
 			return nil
 		}
@@ -95,7 +190,6 @@ func (g *Generator) getLValue(tree antlr.ParseTree) ir.Value {
 
 	case *parser.PostfixExpressionContext:
 		// Postfix calls (foo()) are not l-values.
-		// Check if any op is a call.
 		for _, op := range ctx.AllPostfixOp() {
 			if op.LPAREN() != nil {
 				return nil
